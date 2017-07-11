5ad98e7476494ba66c3d6d5d90594caf7dd7e46d
Kieran <i@kieran.top>
2017-07-11 10:53:55 +0800

    add process port

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ proxy.on('error', function (err, req, res) {
 
 http.createServer(function (req, res) {
   proxy.web(req, res, {target: "https://www.google.com.hk" + req.url});
-}).listen(5000);
+}).listen(process.env.PORT || '5000');
