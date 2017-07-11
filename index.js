f6f11ca41e581a901f65a1bdaffd51e69befff0c
Kieran <i@kieran.top>
2017-07-11 10:29:08 +0800

    Happy😀

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require("http");
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer({
+  ignorePath: false,
+  changeOrigin: true,
+  prependPath: false
+  });
+
+proxy.on('error', function (err, req, res) {
+});
+
+http.createServer(function (req, res) {
+  proxy.web(req, res, {target: "https://www.google.com.hk" + req.url});
+}).listen(8080);
